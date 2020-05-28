986dede6555c3819f3e4776077367acb64e82d45
Kamil M <kamilmaz9206@gmail.com>
2020-05-28 21:54:05 +0200

    change description in main.js

--- main.js
+++ main.js
@@ -1,12 +1,9 @@
-// KOLOR GENEROWANY W ZALEŻNOŚCI OD TEGO GDZIE KLIKNIEMY
+// COLOR GENERATED DEPENDING ON WHERE WE CLICK
 
 // jeśli wartość dla x (event.clientX) i y (event.clientY) jest parzysta (np. 100,122)  to kolor czerwony
 // jesli wartość dla x i y jest nieparzysta to kolor niebieski
 // jeśli jeden z kolorów jest parzysty a drugi nieparzysty to kolor zielony
 
-// let red = Math.floor(Math.random() * 255);
-// let green = Math.floor(Math.random() * 255);
-// let blue = Math.floor(Math.random() * 255);
 
 document.body.addEventListener('click', (e) => {
     const colors = getColors(e);
